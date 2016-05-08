28be23542cce1a9a81cb66f73aaaf9dfb8b8a23b
dfujise <dfujise@yahoo.com.br>
2016-05-08 18:50:59 -0300

    Arquivos Aula 3

--- app/src/main/java/androidpro/com/br/appaula3/MainActivity.java
+++ app/src/main/java/androidpro/com/br/appaula3/MainActivity.java
@@ -2,6 +2,9 @@ package androidpro.com.br.appaula3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Animation fadein = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.fadein);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.startAnimation(fadein);
     }
 }
